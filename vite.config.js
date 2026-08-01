c4d047eeed8d6e3a72745a369fd33383daee4405
berefire <berenice.prado.villalobos@gmail.com>
2026-08-01 04:27:40 -0600

    chore: add Tailwind CSS and Vite configuration
    - Added Tailwind CSS and its Vite plugin as dependencies in package.json.
    - Created a new vite.config.js file to configure Vite with Tailwind CSS and set up path aliases for easier imports.

---
+++ vite.config.js
@@ -0,0 +1,18 @@
+import { defineConfig } from "vite";
+import tailwindcss from "@tailwindcss/vite";
+import { resolve } from "node:path";
+
+export default defineConfig({
+  plugins: [
+    tailwindcss(),
+  ],
+  base: "/space-tourism-multi-page-website/",
+  resolve: {
+    alias: {
+      "@": resolve(__dirname, "src"),
+      "@assets": resolve(__dirname, "src/assets"),
+      "@styles": resolve(__dirname, "src/styles"),
+      "@js": resolve(__dirname, "src/js"),
+    },
+  },
+});
